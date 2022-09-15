c5e3e24694f1a2400628252408cc02e5533ec7fd
NohJinHo <ho199590@gmail.com>
2022-09-15 16:05:17 +0900

    UPDATE

---
+++ Assets/1.Script/Common/ScoreHandler.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ScoreHandler : MonoBehaviour
+{
+    #region 변수
+    [SerializeField]
+    int MaxScore;
+    int curScore = 0;
+
+    [Header("화면 잠금")]
+    [SerializeField]
+    GameObject ScreenSaver;
+    
+    [Header("스코어 변화 UI")]
+    [SerializeField]
+    Sprite scoreCase;
+    [SerializeField]
+    Sprite scoreFill;
+    [SerializeField]
+    GameObject fillParticle;
+
+    [Header("오디오 클립")]
+    [SerializeField]
+    AudioClip[] clips;    
+    AudioSource sources;
+
+    [Header("클리어 파티클")]
+    [SerializeField]
+    GameObject[] particles;
+
+    //완료 이벤트
+    public event System.Action SceneComplete;
+    #endregion
+
+    #region 함수
+    private void Awake()
+    {
+        sources = GetComponent<AudioSource>();
+
+        for (int i = 0; i < MaxScore; i++)
+        {
+            GameObject gg = new GameObject();
+            gg.AddComponent<UnityEngine.UI.Image>();
+            gg.transform.SetParent(transform);
+            gg.transform.localScale = Vector3.one * 1.5f;
+            gg.transform.position = transform.position;
+            gg.transform.localRotation = Quaternion.Euler(Vector3.zero);
+        }
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            if (transform.GetChild(i).GetComponent<UnityEngine.UI.Image>() != null)
+            {
+                transform.GetChild(i).GetComponent<UnityEngine.UI.Image>().sprite = scoreCase;
+            }
+        }
+
+        SceneComplete += Comp;
+    }
+
+    public void SetScore()
+    {   
+        var filled = Instantiate(fillParticle);
+        filled.transform.position = transform.GetChild(curScore).position;
+        transform.GetChild(curScore).GetComponent<UnityEngine.UI.Image>().sprite = scoreFill;
+        transform.GetChild(curScore).GetComponent<UnityEngine.UI.Image>().DOFade(1, 1f).From(0);
+
+        if(curScore < MaxScore - 1)
+        {
+            SoundPlay(0);
+            curScore++;
+        }
+        else
+        {
+            SoundPlay(1);
+            OnComplete();
+        }
+    }
+
+    public void OnComplete()
+    {
+        SceneComplete?.Invoke();
+    }
+
+    IEnumerator ClearParticle()
+    {
+        while (true)
+        {
+            int num = Random.Range(4, 9);
+            for (int i = 0; i < num; i++)
+            {
+                var ex = Instantiate(particles[Random.Range(0, particles.Length)]);
+                if (!transform.root.GetComponent<Canvas>())
+                {
+                    ex.transform.SetParent(transform.root);
+                }
+                ex.transform.localPosition = new Vector3(Random.Range(-8, 8), Random.Range(-5, 5), 0);
+                ex.transform.localScale = Vector3.one * 1.5f;
+            }
+            yield return new WaitForSeconds(1f);
+        }
+    }
+    #endregion
+
+    #region
+    public void SoundPlay(int num)
+    {
+        if (num < clips.Length)
+        {
+            sources.PlayOneShot(clips[num]);
+        }
+    }
+
+    public void Comp()
+    {
+        ScreenSaver?.SetActive(true);
+        StartCoroutine(ClearParticle());
+    }
+    #endregion
+}
---
+++ Assets/1.Script/Scriptable/HangeulSpriteContainer.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObject/HangeulIMG", fileName = "ImageCon")]
+public class HangeulSpriteContainer : ScriptableObject
+{
+    #region 변수
+    [SerializeField]
+    List<Sprite> giyeog, nieun, digeud, lieul, mieum, bieub, sios, ieung, jieuj, chieuch, kieuk, tieut, pieup, hieuh;
+    private Dictionary<int, List<Sprite>> SpriteDictionary = new Dictionary<int, List<Sprite>>();
+
+    [Header("자모음")]
+    [SerializeField]
+    private List<Sprite> Consonant;
+    [SerializeField]
+    private List<Sprite> Vowel;
+    #endregion
+
+    #region 함수
+
+    public void Init()
+    {
+        SpriteDictionary.Clear();
+        SpriteDictionary.Add(0, giyeog);
+        SpriteDictionary.Add(1, nieun);
+        SpriteDictionary.Add(2, digeud);
+        SpriteDictionary.Add(3, lieul);
+        SpriteDictionary.Add(4, mieum);
+        SpriteDictionary.Add(5, bieub);
+        SpriteDictionary.Add(6, sios);
+        SpriteDictionary.Add(7, ieung);
+        SpriteDictionary.Add(8, jieuj);
+        SpriteDictionary.Add(9, chieuch);
+        SpriteDictionary.Add(10, kieuk);
+        SpriteDictionary.Add(11, tieut);
+        SpriteDictionary.Add(12, pieup);
+        SpriteDictionary.Add(13, hieuh);
+    }
+
+    public Dictionary<int, List<Sprite>> GetDictionary(){return SpriteDictionary;}
+
+    public List<Sprite> GetConsonant(){return Consonant;}
+
+    public List<Sprite> GetVowel(){return Vowel;}
+    #endregion
+}
