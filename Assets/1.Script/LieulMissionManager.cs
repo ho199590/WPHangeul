6c32a41733ce934e41d6c7bee7e343be6983386a
HyeonJin67 <dudhkd0607@naver.com>
2022-09-21 16:05:26 +0900

    에셋추가 및 ㄹ미션씬 생성
    ㄹ미션씬에 쓰일 에셋들(BrickTown, CartoonShop, PACKAGES, SwampHouse) 추가

---
+++ Assets/1.Script/LieulMissionManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LieulMissionManager : MonoBehaviour
+{
+
+    private void OnMouseEnter()
+    {
+        print("OnMouseEnter");
+    }
+    private void OnMouseOver()
+    {
+        print("OnMouseOver");
+
+    }
+    private void OnMouseDown()
+    {
+        print("OnMouseDown");
+    }
+    private void OnMouseUp()
+    {
+        print("OnMouseUp");
+    }
+    private void OnMouseUpAsButton()
+    {
+        print("OnMouseUpAsButton");
+    }
+    private void OnMouseExit()
+    {
+        print("OnMouseExit");
+    }
+    
+    
+}
---
+++ Assets/StoreAssetsPack/CartoonShop/Doc/Readme.txt
@@ -0,0 +1,15 @@
+Hello!
+
+Dear customer,
+
+Thank you for purchase my 3D asset Cartoon Shop. The package is now updated !!!
+UPDATE includes:
+- Atlas Map for all 3D objects
+- All shops and buildings have loop animation
+- Non-animated objects are still in the pack
+- Add more buildings: hotel, appartments, clock tower and a ramen noodle restaurant
+
+The old package version "Mini Toon Store" will be not supported any more.
+If you need any help, pls contact me at souchenki@gmail.com
+
+Thanks and Best Regards,
