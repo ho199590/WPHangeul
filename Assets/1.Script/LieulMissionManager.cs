db82a95a495760d48a56b1ef30c1acabc3757f69
HyeonJin67 <dudhkd0607@naver.com>
2022-09-22 11:36:30 +0900

    ㄹ미션씬 스크립트 추가
    OnMouse일때 UI의 버튼 처럼 반응하도록 오브젝트 색깔 변경

--- Assets/1.Script/LieulMissionManager.cs
+++ Assets/1.Script/LieulMissionManager.cs
@@ -4,32 +4,28 @@ using UnityEngine;
 
 public class LieulMissionManager : MonoBehaviour
 {
-
     private void OnMouseEnter()
     {
         print("OnMouseEnter");
-    }
-    private void OnMouseOver()
-    {
-        print("OnMouseOver");
-
+        GetComponent<MeshRenderer>().material.color = new Color(1,1,1);
     }
     private void OnMouseDown()
     {
         print("OnMouseDown");
+        GetComponent<MeshRenderer>().material.color = new Color(0.1f, 1, 1);
     }
     private void OnMouseUp()
     {
         print("OnMouseUp");
+        GetComponent<MeshRenderer>().material.color = new Color(1,1,1);
     }
     private void OnMouseUpAsButton()
     {
-        print("OnMouseUpAsButton");
+        print("OnMouseUpAsButton?");
     }
     private void OnMouseExit()
     {
         print("OnMouseExit");
+        GetComponent<MeshRenderer>().material.color = new Color(0.7f, 0.7f, 0.7f);
     }
-    
-    
 }
