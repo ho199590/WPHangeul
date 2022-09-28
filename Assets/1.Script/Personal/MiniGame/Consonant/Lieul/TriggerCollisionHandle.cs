95d441dad3a69b5efa4ba48eadf96c5ee79e49f6
HyeonJin67 <dudhkd0607@naver.com>
2022-09-28 16:41:11 +0900

    MazeGenerator에셋 추가 및 ㄹ미션씬 추가
    미로관련 무료 에셋 추가
    ㄹ의 길찾기 씬 추가

---
+++ Assets/1.Script/Personal/MiniGame/Consonant/Lieul/TriggerCollisionHandle.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerCollisionHandle : MonoBehaviour
+{
+    private Rigidbody mRigidBody;
+    private AudioSource mAudioSource;
+    public AudioClip JumpSound;
+    public AudioClip HitSound;
+    public GameObject ViewCamera;
+    void Start()
+    {
+        mRigidBody = GetComponent<Rigidbody>();
+        mAudioSource = GetComponent<AudioSource>();
+    }
+    void FixedUpdate()
+    {
+        if (mRigidBody != null)
+        {
+            if (Input.GetButton("Horizontal"))
+            {
+                mRigidBody.AddTorque(Vector3.back * Input.GetAxis("Horizontal")*10);
+            }
+            if (Input.GetButton("Vertical"))
+            {
+                mRigidBody.AddTorque(Vector3.right * Input.GetAxis("Vertical")*10);
+            }
+            if (Input.GetButtonDown("Jump"))
+            {
+                if (mAudioSource != null && JumpSound != null)
+                {
+                    mAudioSource.PlayOneShot(JumpSound);
+                }
+                mRigidBody.AddForce(Vector3.up*200);
+            }
+        }
+        if (ViewCamera != null)
+        {
+            Vector3 direction = (Vector3.up*2+Vector3.back)*2;
+            RaycastHit hit;
+            Debug.DrawLine(transform.position, transform.position+direction, Color.red);
+            if (Physics.Linecast(transform.position, transform.position+direction, out hit))
+            {
+                ViewCamera.transform.position = hit.point;
+            }
+            else
+            {
+                ViewCamera.transform.position = transform.position+direction;
+            }
+            ViewCamera.transform.LookAt(transform.position);
+        }
+    }
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision != null)
+        {
+            print("온콜리전엔터");
+            if (mAudioSource != null && HitSound != null && collision.relativeVelocity.y > .5f)
+            {
+                mAudioSource.PlayOneShot(HitSound, collision.relativeVelocity.magnitude);
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other != null)
+        {
+            print("온트리거엔터");
+            other.gameObject.SetActive(false);
+        }
+    }
+}
