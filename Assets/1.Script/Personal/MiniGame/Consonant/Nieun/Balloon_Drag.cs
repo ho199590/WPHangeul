f72352142fa2f8a056df0fe2b5b180fa9fb3ad20
Leesuhwa <56902250+Leesuhwa@users.noreply.github.com>
2022-09-28 17:49:29 +0900

    d
    d

--- Assets/1.Script/Personal/MiniGame/Consonant/Nieun/Balloon_Drag.cs
+++ Assets/1.Script/Personal/MiniGame/Consonant/Nieun/Balloon_Drag.cs
@@ -5,15 +5,14 @@ using UnityEngine;
 public class Balloon_Drag : MonoBehaviour
 {
     [SerializeField]
-    GameObject Balloon;//움직일 풍선 오브젝트
-
+    GameObject Balloon;//공중에 떠다니는 풍선 오브젝트
     private void Start()
     {
 
     }
     private void OnMouseUp()
     {
-        print("드래그중 아님");
+        /*Random_Move.isMove = true;*/
     }
     private void OnMouseDown()
     {
@@ -26,6 +25,5 @@ public class Balloon_Drag : MonoBehaviour
             = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 40);
         //마우스 좌표를 스크린 투 월드로 바꾸고 이 객체의 위치로 설정해 준다.
         this.transform.position = Camera.main.ScreenToWorldPoint(mousePosition);
-        Balloon.GetComponent<Rigidbody>().useGravity = false;
     }
 }
