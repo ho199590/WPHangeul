75472cadd2249972ca0c90525ddb21b6d297453c
HyeonJin67 <dudhkd0607@naver.com>
2022-10-05 11:38:41 +0900

    ㄹ길탐험 사용자시점 이동

--- Assets/1.Script/Personal/MiniGame/Consonant/Lieul/TriggerCollisionHandle.cs
+++ Assets/1.Script/Personal/MiniGame/Consonant/Lieul/TriggerCollisionHandle.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//collision, trigger 충돌
 public class TriggerCollisionHandle : MonoBehaviour
 {
     private Rigidbody mRigidBody;
@@ -20,11 +20,13 @@ public class TriggerCollisionHandle : MonoBehaviour
         {
             if (Input.GetButton("Horizontal"))
             {
-                mRigidBody.AddTorque(Vector3.back * Input.GetAxis("Horizontal")*10);
+                //mRigidBody.AddTorque(Vector3.back * Input.GetAxis("Horizontal")*10);
+                mRigidBody.AddForce(Vector3.right *Input.GetAxis("Horizontal")*10); //Rigidbody의 Use Gravity사용해줘야 제대로 적용
             }
             if (Input.GetButton("Vertical"))
             {
-                mRigidBody.AddTorque(Vector3.right * Input.GetAxis("Vertical")*10);
+                //mRigidBody.AddTorque(Vector3.right * Input.GetAxis("Vertical")*10);
+                mRigidBody.AddForce(Vector3.forward *Input.GetAxis("Vertical")*10); //Rigidbody의 Use Gravity사용해줘야 제대로 적용
             }
             if (Input.GetButtonDown("Jump"))
             {
@@ -37,23 +39,18 @@ public class TriggerCollisionHandle : MonoBehaviour
         }
         if (ViewCamera != null)
         {
-            
             //Vector3 direction = (Vector3.up*2+ Vector3.back*4);
             RaycastHit hit;
             Debug.DrawLine(transform.position, transform.position, Color.red);
             if (Physics.Linecast(transform.position, transform.position, out hit))
             {
                 ViewCamera.transform.position = hit.point;
-
             }
             else
             {
                 ViewCamera.transform.position = transform.position;
-                
             }
-            //ViewCamera.transform.forward = transform.forward;
-            //ViewCamera.transform.rotation = Quaternion.Lerp(ViewCamera.transform.rotation, Quaternion.LookRotation(transform.position), Time.deltaTime);
-            ViewCamera.transform.LookAt(transform.position + transform.forward);
+            ViewCamera.transform.LookAt(transform.position  + transform.forward);
         }
     }
     
@@ -62,10 +59,11 @@ public class TriggerCollisionHandle : MonoBehaviour
         if(collision != null)
         {
             print("온콜리전엔터" + collision);
-            if (mAudioSource != null && HitSound != null && collision.relativeVelocity.y > .5f)
-            {
-                mAudioSource.PlayOneShot(HitSound, collision.relativeVelocity.magnitude);
-            }
+            //공 확대해주는 거
+            //if (mAudioSource != null && HitSound != null && collision.relativeVelocity.y > .5f)
+            //{
+            //    mAudioSource.PlayOneShot(HitSound, collision.relativeVelocity.magnitude);
+            //}
         }
     }
     private void OnTriggerEnter(Collider other)
@@ -73,7 +71,12 @@ public class TriggerCollisionHandle : MonoBehaviour
         if (other != null)
         {
             print("온트리거엔터"+other);
+            transform.rotation = Quaternion.Slerp(transform.rotation, other.transform.rotation, Time.deltaTime*50);
             other.gameObject.SetActive(false);
+            if (other.gameObject.name.Contains("Fin"))
+            {
+                print("도착!");
+            }
         }
     }
 }
