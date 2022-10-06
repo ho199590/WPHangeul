c4a2c22724225f5b9f5813eaa8eee368ae469f31
Leesuhwa <56902250+Leesuhwa@users.noreply.github.com>
2022-10-06 11:45:37 +0900

    dd
    dd

--- Assets/1.Script/Personal/MiniGame/Consonant/Nieun/Balloon/Balloon_Touch.cs
+++ Assets/1.Script/Personal/MiniGame/Consonant/Nieun/Balloon/Balloon_Touch.cs
@@ -7,19 +7,20 @@ public class Balloon_Touch : MonoBehaviour
     [SerializeField]
     GameObject Animal;//동물 오브젝트
     int BalloonTouch=0;//풍선 터치 카운트
-    SpeakerHandler speakerHandler;
     public Vector3 BalloonPosition;
+    Balloon_Move ballMove;
     private void Start()
     {
-        speakerHandler = FindObjectOfType<SpeakerHandler>();
+        ballMove = GetComponentInParent<Balloon_Move>();
     }
     private void OnMouseDown()
     {
         BalloonTouch++;
-        speakerHandler.SoundByNum2(0);
         if(BalloonTouch == 3)//다섯번 클릭했을때 
         {
+            ballMove.enabled = false;
             TouchClear();
+            ColOnOff();
             BalloonTouch = 0;
         }
     }
@@ -28,6 +29,9 @@ public class Balloon_Touch : MonoBehaviour
         Animal.GetComponent<Rigidbody>().useGravity = true;
         gameObject.SetActive(false);
     }
-
+    private void ColOnOff()
+    {
+        Animal.GetComponent<Collider>().enabled = true;
+    }
     
 }
