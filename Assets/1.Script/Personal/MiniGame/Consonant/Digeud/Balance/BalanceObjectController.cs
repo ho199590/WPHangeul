317eea97743f1f0cdc6c65c2162c69b15e64d094
NohJinHo <ho199590@gmail.com>
2022-10-07 14:55:54 +0900

    업데이트

--- Assets/1.Script/Personal/MiniGame/Consonant/Digeud/Balance/BalanceObjectController.cs
+++ Assets/1.Script/Personal/MiniGame/Consonant/Digeud/Balance/BalanceObjectController.cs
@@ -9,6 +9,8 @@ public class BalanceObjectController : MonoBehaviour
     private int weight;
     private int index;
 
+    public bool Clone = true;
+
     [SerializeField]
     Transform SpawnPoint;
 
@@ -38,9 +40,13 @@ public class BalanceObjectController : MonoBehaviour
         }
         screenSpace = Camera.main.WorldToScreenPoint(transform.position);
         offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenSpace.z));
-
-        var sig = Instantiate(transform);
-        sig.position = SpawnPoint.position;
+        
+        if (Clone)
+        {
+            var sig = Instantiate(transform);
+            sig.position = SpawnPoint.position;
+            sig.GetComponent<BalanceObjectController>().Clone = false;
+        }
     }
 
     private void OnMouseDrag()
