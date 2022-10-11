eabd1c25b1e3a8c691114024c68408b9083bbe6c
Leesuhwa <56902250+Leesuhwa@users.noreply.github.com>
2022-10-11 16:33:09 +0900

    ㅇㅇ
    ㅇ

--- Assets/1.Script/Personal/MiniGame/Consonant/Nieun/Balloon/Balloon_Touch.cs
+++ Assets/1.Script/Personal/MiniGame/Consonant/Nieun/Balloon/Balloon_Touch.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Balloon_Touch : MonoBehaviour
 {
+    #region 변수
     [SerializeField]
-    GameObject Animal;//동물 오브젝트
+    GameObject animal;//동물 오브젝트
     int BalloonTouch=0;//풍선 터치 카운트
     public Vector3 BalloonPosition;
-    Balloon_Move ballMove;
     SpeakerHandler speakerHandler;
+    //[풍선이 터지면 동물작아지는 변수]
+    private float size = 0.8f; //원하는 사이즈
+    private float speed = 1f; //작아 질때의 속도
+    private float time = 0;
+    #endregion
+    #region
+
     private void Start()
     {
-        ballMove = GetComponentInParent<Balloon_Move>();
         speakerHandler = FindObjectOfType<SpeakerHandler>();
     }
     private void OnMouseDown()
@@ -20,7 +26,7 @@ public class Balloon_Touch : MonoBehaviour
         BalloonTouch++;
         if(BalloonTouch == 3)//다섯번 클릭했을때 
         {
-            ballMove.enabled = false;
+            StartCoroutine(Down());
             TouchClear();
             ColOnOff();
             BalloonTouch = 0;
@@ -29,12 +35,33 @@ public class Balloon_Touch : MonoBehaviour
     protected void TouchClear()//풍선 낙하 
     {
         speakerHandler.SoundByNum2(0);
-        Animal.GetComponent<Rigidbody>().isKinematic = false;
-        gameObject.SetActive(false);
+        animal.GetComponent<Rigidbody>().isKinematic = false;
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
     }
     private void ColOnOff()//바닦과 충돌 하기위한 함수
     {
-        Animal.GetComponent<Collider>().enabled = true;
+        animal.GetComponent<Collider>().enabled = true;
     }
-    
+
+    private IEnumerator Down()
+    {
+        while (animal.GetComponent<Transform>().localScale.x > size)
+        {
+            animal.GetComponent<Transform>().localScale = new Vector3(animal.GetComponent<Transform>().localScale.x - 1f * speed * Time.deltaTime, animal.GetComponent<Transform>().localScale.y - 1f * speed * Time.deltaTime, animal.GetComponent<Transform>().localScale.z - 1f * speed * Time.deltaTime);
+            time += Time.deltaTime;
+            if (animal.GetComponent<Transform>().localScale.x <= size)
+            {
+                time = 0;
+                break;
+            }
+            yield return null;
+        }
+
+    }
+    public void SizeReset()
+    {
+        animal.GetComponent<Transform>().localScale = new Vector3(5f, 5f, 5f);
+        print("실행");
+    }
+    #endregion
 }
