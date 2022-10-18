a1d1d90401f899e6e72051261e255dd72f327f3b
HyeonJin67 <dudhkd0607@naver.com>
2022-10-18 11:12:56 +0900

    ㄹ스크립트 정리

--- Assets/1.Script/Personal/MiniGame/Consonant/Lieul/NaviMoveManager.cs
+++ Assets/1.Script/Personal/MiniGame/Consonant/Lieul/NaviMoveManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.AI;
 using System;
 //방향키대신 공(Mover)의 자동 움직임을 담당해주는 NavMesh기능  
-public class NaviMoveHandler : MonoBehaviour
+public class NaviMoveManager : MonoBehaviour
 {
     [Tooltip ("순서대로 도착할 곳의 위치(NavMesh의 타겟)를 넣어주세요")]
     [SerializeField]
@@ -21,9 +21,6 @@ public class NaviMoveHandler : MonoBehaviour
     NavMeshAgent agent;
     int index;
     int count = 0;
-
-    public event System.Action<int> Quiz2CountCheck;
-
     //콜라이더를 OnTrigger로 만났을때 방향 NavMesh의 타겟(도착지점)을 다음 타겟으로 바꿔주는 프로퍼티 
     public int DestinationIndex
     {
@@ -43,6 +40,7 @@ public class NaviMoveHandler : MonoBehaviour
     {
         agent.SetDestination(destination); //NavMesh의 타겟(도착지점) 지정
     }
+    //공이 골목을 지나가면서 통과해서 만나 지나가게 될 충돌처리
     public void OnTriggerEnter(Collider other) //Collider에 Is Trigger 체크되어있어야 충돌 //통과가능(물리연산X)
     {
         if (other != null)
@@ -71,6 +69,19 @@ public class NaviMoveHandler : MonoBehaviour
             //transform.rotation = Quaternion.Lerp(transform.rotation, other.transform.rotation, Time.deltaTime * 50);
         }
     }
+    //코너에서 만난 collider의 설정되어 있는 회전 방향으로 공도 똑같이 회전
+    private void OnCollisionEnter(Collision collision) //Collider에 Is Trigger 체크안되어 있어야 충돌 //통과안됨(물리연산O)
+    {
+        if (collision != null)
+        {
+            print("온콜리전엔터" + collision.gameObject.name);
+            if (index < 4)
+            {
+                DestinationIndex = ++index; //다음 타겟으로 인덱스값 +1해서 넘겨주기
+                print(index);
+            }
+        }
+    }
     void Quiz1Right()
     {
         print("이벤트 실행 테스트1");
@@ -97,16 +108,5 @@ public class NaviMoveHandler : MonoBehaviour
             }
         }
     }
-    private void OnCollisionEnter(Collision collision) //Collider에 Is Trigger 체크안되어 있어야 충돌 //통과안됨(물리연산O)
-    {
-        if (collision != null)
-        {
-            print("온콜리전엔터" + collision.gameObject.name);
-            if (index < 4)
-            {
-                DestinationIndex = ++index; //다음 타겟으로 인덱스값 +1해서 넘겨주기
-                print(index);
-            }
-        }
-    }
+    
 }
