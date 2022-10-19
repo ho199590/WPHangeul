c2ca93c684759f8f3cc3789d3bca16b9459494b6
Leesuhwa <56902250+Leesuhwa@users.noreply.github.com>
2022-10-19 17:59:13 +0900

    dd
    dd

---
+++ Assets/1.Script/Personal/MiniGame/Consonant/Nieun/Anim/Animation_Controller.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation_Controller : MonoBehaviour
+{
+    private Animator animator;
+    private void Awake()
+    {
+      
+    }
+/*    private void OnPlay()
+    {
+        animator.SetInteger("")
+    }*/
+}
---
+++ Assets/1.Script/Personal/MiniGame/Consonant/Nieun/Test_Property.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Test_Property : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        Test_Property2.Num();
+    }
+}
