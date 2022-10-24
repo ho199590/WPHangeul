4995e3557167206fdf160369a5f6744c19fc53f3
HyeonJin67 <dudhkd0607@naver.com>
2022-10-24 10:39:41 +0900

    ㄹ 프로퍼티&이벤트 완성
    이제 공통스크립트로 자음과 각 퀘스트별 퀴즈만 변경해서 사용가능

--- Assets/1.Script/Personal/MiniGame/Consonant/Lieul/ActiveQuizObjectHandle.cs
+++ Assets/1.Script/Personal/MiniGame/Consonant/Lieul/ActiveQuizObjectHandle.cs
@@ -4,26 +4,26 @@ using UnityEngine;
 
 public class ActiveQuizObjectHandle : MonoBehaviour
 {
-    [Tooltip ("퀘스트별 퀴즈시작때 <QuizTouchHandle>스크립트가 들어있는 활성화가 필요한 오브젝트들만 다 넣어주세요")]
+    [Tooltip("퀘스트별 퀴즈시작때 <QuizTouchHandle>스크립트가 들어있는 활성화가 필요한 오브젝트들만 다 넣어주세요")]
     [SerializeField]
     GameObject[] quizObjects; //퀘스트별 퀴즈시작시 활성화해줘야 할 오브젝트
     [Tooltip("<QuizTouchHandle>스크립트가 들어있고 퀘스트별 퀴즈시작때 떨어뜨려줘야 할 오브젝트들만 다 넣어주세요")]
     [SerializeField]
     Rigidbody[] drops; //퀘스트별 퀴즈시작시 떨어뜨려줘야 할 오브젝트
-
-
+    int count;
     private void OnTriggerEnter(Collider other)
     {
-        if(quizObjects != null)
+        FindObjectOfType<QuizManager>().QuizOrder = count; //몇번째 퀴즈인지 카운트용 프로퍼티 호출
+        if (quizObjects != null)
         {
             for (int i = 0; i < quizObjects.Length; i++)
             {
                 quizObjects[i].SetActive(true);
             }
         }
-        if(drops != null)
+        if (drops != null)
         {
-            for(int j =0; j < drops.Length; j++)
+            for (int j = 0; j < drops.Length; j++)
             {
                 drops[j].useGravity = true;
             }
