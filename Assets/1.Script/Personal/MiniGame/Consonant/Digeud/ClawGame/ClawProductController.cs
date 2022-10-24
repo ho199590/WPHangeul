19473067306c78179d1e7f041021f568e2d533ee
NohJinHo <ho199590@gmail.com>
2022-10-24 09:05:44 +0900

    폴더 준비

---
+++ Assets/1.Script/Personal/MiniGame/Consonant/Digeud/ClawGame/ClawProductController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClawProductController : MonoBehaviour
+{
+    #region 변수
+    [Header("뽑기 목록")]
+    [SerializeField]
+    List<GameObject> Products = new List<GameObject>();
+
+    //상품 갯수
+    [SerializeField]
+    int ProductNum;
+
+    [Header("뽑기 상품 배치 위치 조정")]
+    [SerializeField]
+    Vector3 center;
+    [SerializeField]
+    Vector3 size;
+    #endregion
+
+    #region
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            ProductSetting();
+        }
+    }
+    public void ProductSetting()
+    {
+        Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), Random.Range(-size.y / 2, size.y / 2), Random.Range(-size.z / 2, size.z / 2));
+
+        int index = Random.Range(0, Products.Count);
+
+        GameObject product = Instantiate(Products[index], pos, Quaternion.identity, transform);
+        product.transform.LookAt(center);
+
+
+        product.name = "생성" +  ProductNum.ToString();
+        product.GetComponent<JointTest>().PrefabNumber = index;
+        ProductNum++;
+    }
+
+
+    public void ResetDropProducts(Transform Target, int num)
+    {
+        Target.gameObject.GetComponent<Collider>().enabled = false;
+        Target.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+
+        GameObject go = Instantiate(Products[num], Target.transform.position, Target.transform.rotation, transform) as GameObject;
+        go.GetComponent<JointTest>().PrefabNumber = num;
+
+        Destroy(Target.gameObject);
+    }
+    #endregion
+
+
+
+
+
+
+
+    //화면에 그림 그리기
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(0, 0, 1, 0.5f);
+        Gizmos.DrawCube(center, size);
+    }
+}
---
+++ Assets/1.Script/Personal/MiniGame/Consonant/Digeud/ClawGame/JointTest.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JointTest : MonoBehaviour
+{
+    #region 변수
+    ClawController clawController;
+    ClawProductController product;
+
+    Transform parent;
+
+    public int PrefabNumber;
+    #endregion
+
+    private void Start()
+    {
+        clawController = FindObjectOfType<ClawController>();
+        clawController.MagnetPutDown += PutDownObject;
+
+        product = FindObjectOfType<ClawProductController>();
+
+        parent = transform.parent;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("LKJ" + collision.gameObject.name);
+        if (collision.gameObject.name != "Stage")
+        {
+            if (GetComponent<Joint>())
+            {
+                if (GetComponent<Joint>().connectedBody == null && collision.transform.parent != parent)
+                {
+                    transform.SetParent(collision.transform);
+                    GetComponent<Joint>().connectedBody = collision.rigidbody;
+                    clawController.AddBodyPart(gameObject);
+                    gameObject.layer = 0;
+
+                    GetComponent<Joint>().autoConfigureConnectedAnchor = false;
+                    //자식 갯수로 판별시 꾸미기 어려움이 있음
+                    //GetComponent<Joint>().connectedAnchor = collision.transform.childCount < 3 ? Vector3.down * 0.5f : Vector3.down * 2.1f;                    
+                    GetComponent<Joint>().connectedAnchor = !collision.transform.GetComponent<MagnetHandler>() ? Vector3.down * 0.5f : Vector3.down * 2.1f;
+                    //자석 올리기 이벤트 호출
+                    clawController.MagnetCollisionInvoke();
+                }
+                else if (GetComponent<Joint>().connectedBody != collision.rigidbody)
+                {
+                    clawController.MagnetCollisionInvoke();
+                }
+            }
+            //GetComponent<Joint>(). = 50;
+        }
+        if (!GetComponent<Joint>())
+        {
+            if(GetComponent<Rigidbody>().velocity.magnitude < 1 && GetComponent<Rigidbody>().velocity.y >= 0)
+            {
+                //여기서 호출을..?
+                print(collision.gameObject.name);
+                product.ResetDropProducts(transform, PrefabNumber);
+            }
+        }
+    }
+
+    // 잡혀있는 오브젝트 떨어트리기
+    public void PutDownObject()
+    {
+        if (transform.parent.GetComponent<Rigidbody>())
+        {
+            transform.SetParent(null);
+
+            int r = Random.Range(10, 20);
+            Vector3 RandomForce = new Vector3(Random.Range(-r, r), -2, Random.Range(-r, r));
+            GetComponent<Rigidbody>().AddForce(RandomForce);
+            
+
+            GetComponent<Joint>().breakForce = 0;
+            transform.parent = parent;
+        }
+    }
+
+
+    // 오브젝트가 사라질 경우 이벤트 제외
+    private void OnDestroy()
+    {
+        clawController.MagnetPutDown -= PutDownObject;
+    }
+}
