e912806b5e5485d77ca4b299b7d0dbbfdd5e03fd
HyeonJin67 <dudhkd0607@naver.com>
2022-10-31 13:19:28 +0900

    Revert "Revert "Revert "Revert "ㄹdragNdrop스크립트 충돌처리 추가""""
    This reverts commit fa46bfdb282eb35c3cd2e07ebfc61ada2069f951.

--- Assets/1.Script/Personal/MiniGame/Consonant/Lieul/DragNDropHandle.cs
+++ Assets/1.Script/Personal/MiniGame/Consonant/Lieul/DragNDropHandle.cs
@@ -9,6 +9,9 @@ public class DragNDropHandle : MonoBehaviour
     Collider collider;
     [SerializeField]
     Collider lieulPosi;
+    bool check = false;
+    [SerializeField]
+    GameObject active;
   
     private void Start()
     {
@@ -33,6 +36,9 @@ public class DragNDropHandle : MonoBehaviour
         if(collider != null && collider.gameObject == lieulPosi.gameObject)
         {
             print("충돌체크확인");
+            gameObject.SetActive(false);
+            collider.gameObject.SetActive(false);    
+            active.SetActive(true);
             FindObjectOfType<QuizManager>().AddNRemove = gameObject;
         }
     }
@@ -46,8 +52,4 @@ public class DragNDropHandle : MonoBehaviour
         }
         else return null;
     }
-    //private void OnMouseUp()
-    //{
-        //FindObjectOfType<QuizManager>().AddNRemove = gameObject;
-    //}
 }
