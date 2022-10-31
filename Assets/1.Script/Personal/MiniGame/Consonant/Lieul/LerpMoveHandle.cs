89d503d387cb41dc0eac8301c9714c96656f4560
HyeonJin67 <dudhkd0607@naver.com>
2022-10-31 09:39:00 +0900

    Revert "Revert "ㄹdragNdrop스크립트 충돌처리 추가""
    This reverts commit d57ba1fe1174b38fbfae4fe1608d132d1ac862d8.

---
+++ Assets/1.Script/Personal/MiniGame/Consonant/Lieul/LerpMoveHandle.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LerpMoveHandle : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    //IEnumerator Appear()
+    //{
+    //    while (Vector3.Distance(transform.position, picoPosition.transform.position) > 0.2f)
+    //    {
+    //        transform.position = Vector3.Lerp(transform.position, picoPosition.transform.position, Time.deltaTime * 0.7f);
+    //        yield return new WaitForSeconds(Time.deltaTime);
+    //        if (Vector3.Distance(transform.position, picoPosition.transform.position) <= 0.2f)
+    //        {
+    //            break;
+    //        }
+    //    }
+    //    transform.position = picoPosition.transform.position;
+    //    yield break;
+    //}
+}
