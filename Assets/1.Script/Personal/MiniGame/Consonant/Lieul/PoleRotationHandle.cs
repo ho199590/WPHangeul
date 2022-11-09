5d64baf768c123f40a88769b4fabe962b829bbc2
HyeonJin67 <dudhkd0607@naver.com>
2022-11-09 17:50:49 +0900

    이정표 회전 수정

--- Assets/1.Script/Personal/MiniGame/Consonant/Lieul/PoleRotationHandle.cs
+++ Assets/1.Script/Personal/MiniGame/Consonant/Lieul/PoleRotationHandle.cs
@@ -4,8 +4,28 @@ using UnityEngine;
 //이정표 회전용
 public class PoleRotationHandle : MonoBehaviour
 {
-    private void Update()
+    //private void Update()
+    //{
+    //    transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * 500);
+    //}
+    private void Start()
     {
-        transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * 500);
+        StartCoroutine(PoleRotate());
+    }
+    IEnumerator PoleRotate()
+    {
+        while(transform.rotation.eulerAngles.y > 180)
+        {
+            print("회전코루틴체크"+transform.rotation.eulerAngles.y);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 180, 0), Time.deltaTime *2);
+            yield return new WaitForSeconds(Time.deltaTime);
+            if(transform.rotation.eulerAngles.y <= 180)
+            {
+                print("브레이크체크");
+                break;
+            }
+        }
+        transform.rotation = Quaternion.Euler(0,180,0);
+        yield break;
     }
 }
