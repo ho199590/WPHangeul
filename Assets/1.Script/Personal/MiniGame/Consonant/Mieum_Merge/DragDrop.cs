27f9aaace0692c0c9806048647c37bd864edff77
Leesuhwa <dltnghks3256@gmail.com>
2022-11-09 18:00:25 +0900

    ㅇㅇㅇ
    ㅇㅇ

--- Assets/1.Script/Personal/MiniGame/Consonant/Mieum_Merge/DragDrop.cs
+++ Assets/1.Script/Personal/MiniGame/Consonant/Mieum_Merge/DragDrop.cs
@@ -26,7 +26,7 @@ public class DragDrop : MonoBehaviour
     {
 /*        print("OnMouseDrag");*/
         //마우스 좌표를 받아온다.
-        Vector3 mousePosition = new Vector3(Input.mousePosition.x,Input.mousePosition.y,20);
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x,Input.mousePosition.y,100);
         //마우스 좌표를 스크린 투 월드로 바꾸고 이 객체의 위치로 설정해 준다.
         this.transform.position = Camera.main.ScreenToWorldPoint(mousePosition);
     }
@@ -50,7 +50,7 @@ public class DragDrop : MonoBehaviour
         while(Vector3.Distance(transform.position , dePosition) >3)        
         {
             transform.position = Vector3.MoveTowards(transform.position, dePosition,
-                Time.deltaTime * 40);
+                Time.deltaTime * 50);
             yield return new WaitForSeconds(Time.deltaTime);
         }
         transform.position = dePosition;
