b08a824420783d196697c3850d96a28bd1902b45
bahaimen <113586204+bahaimen@users.noreply.github.com>
2022-11-11 13:55:01 +0900

    인형

---
+++ Assets/Plugins/Tiny Giant Studio/Modular 3D Text/Scripts/MText_Character.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace MText
+{
+    [System.Serializable]
+    public class MText_Character
+    {
+        public char character;
+        public GameObject prefab;
+        public Mesh meshPrefab;
+
+        public int glyphIndex;
+        /// <summary>
+        /// Named advance in typeface
+        /// </summary>
+        public float spacing = 700; 
+
+        public float xOffset;
+        public float yOffset;
+        public float zOffset;
+    }
+}
