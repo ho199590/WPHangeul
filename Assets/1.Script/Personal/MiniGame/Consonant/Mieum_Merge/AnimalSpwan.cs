99face099f80fe0974c6bbeb119c965b34c4f03b
Leesuhwa <dltnghks3256@gmail.com>
2022-11-15 14:02:40 +0900

    몬스터 드래그 수정 완료

--- Assets/1.Script/Personal/MiniGame/Consonant/Mieum_Merge/AnimalSpwan.cs
+++ Assets/1.Script/Personal/MiniGame/Consonant/Mieum_Merge/AnimalSpwan.cs
@@ -5,22 +5,22 @@ using UnityEngine;
 public class AnimalSpwan : MonoBehaviour
 {
     [SerializeField]
-    GameObject[] animalOb;
+    GameObject[] animalOb;  //ai 몬스터 프리팹 저장 변수
     [SerializeField]
-    int obCount;
-    int num = 1;
+    int obCount;            //인스펙터 창에서 몬스터 갯수를 지정 할수있게 선언
+    int num = 1;            //몬스터 생성시 증감 변수
     private void Start()
     {
-        StartCoroutine(MonsterSpwan());
+        StartCoroutine(MonsterSpwan()); //게임 시작시 코루틴 시작
     }
-    IEnumerator MonsterSpwan()
+    IEnumerator MonsterSpwan()          //게임 시작시 ai몬스터 랜덤하게 생성
     {
         while (true)
         {
             GameObject monster = Instantiate(animalOb[Random.Range(0, animalOb.Length)]);
             num++;
-            yield return new WaitForSeconds(5f);
-            if (num == obCount)
+            yield return new WaitForSeconds(5f);    //생성하고 5초 지연 
+            if (num == obCount)                     //일정 숫자를 넘어가면 생성 제한
             {
                 print("갯수제한");
                 StopAllCoroutines();
--- Assets/1.Script/Personal/MiniGame/Consonant/Mieum_Merge/TestScript.cs
+++ Assets/1.Script/Personal/MiniGame/Consonant/Mieum_Merge/TestScript.cs
@@ -4,12 +4,33 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
-    [SerializeField]
-    Transform[] ob;
+    GameObject objectHitPostion;
+    RaycastHit hitRay, hitLayerMask;
 
-    private void Start()
+    private void OnMouseUp()
     {
+/*        this.transform.parent = null;
+        Destroy(objectHitPostion);*/
+    }
 
-        ob = GameObject.Find("Point").GetComponentsInChildren<Transform>();
+    private void OnMouseDown()
+    {
+/*        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hitRay))
+        {
+            objectHitPostion = new GameObject("HitPosition");
+            objectHitPostion.transform.position = hitRay.point;
+            this.transform.SetParent(objectHitPostion.transform);
+        }*/
+    }
+
+    private void OnMouseDrag()
+    {
+        float distance = Camera.main.WorldToScreenPoint(transform.position).z;
+
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+        Vector3 objPos = Camera.main.ScreenToWorldPoint(mousePos);
+        objPos.y = 0.7f;
+        transform.position = objPos;
     }
 }
