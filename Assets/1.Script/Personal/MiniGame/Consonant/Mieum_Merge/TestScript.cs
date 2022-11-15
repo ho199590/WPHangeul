8ab712a67566247e5b249bee93ce99147804b8b7
Leesuhwa <dltnghks3256@gmail.com>
2022-11-15 10:24:18 +0900

    동물 생성 완료

--- Assets/1.Script/Personal/MiniGame/Consonant/Mieum_Merge/TestScript.cs
+++ Assets/1.Script/Personal/MiniGame/Consonant/Mieum_Merge/TestScript.cs
@@ -5,24 +5,11 @@ using UnityEngine;
 public class TestScript : MonoBehaviour
 {
     [SerializeField]
-    GameObject target;
+    Transform[] ob;
 
     private void Start()
     {
-        StartCoroutine(CubeMove());
-    }
-    IEnumerator CubeMove()
-    {
-        while(true)
-        {
-            transform.position = Vector3.Lerp(transform.position, target.transform.position, Time.fixedDeltaTime);
 
-            yield return new WaitForSeconds(Time.deltaTime);
-            if(Vector3.Distance(transform.position , target.transform.position) <= 1f)
-            {
-                print("Á¾·á");
-                yield break;
-            }
-        }
+        ob = GameObject.Find("Point").GetComponentsInChildren<Transform>();
     }
 }
