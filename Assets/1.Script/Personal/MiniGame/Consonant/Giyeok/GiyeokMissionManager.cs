8f424451a7200023d74d6ef16193aca002ab71ee
HyeonJin67 <dudhkd0607@naver.com>
2022-11-16 17:55:07 +0900

    Revert "튜토리얼씬 추가"
    This reverts commit 9dddb80d00adb6d8c60367395f07402629403d2b.

--- Assets/1.Script/Personal/MiniGame/Consonant/Giyeok/GiyeokMissionManager.cs
+++ Assets/1.Script/Personal/MiniGame/Consonant/Giyeok/GiyeokMissionManager.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using System.Linq;
 using UnityEngine.UI;
 using System;
-using UnityEngine.SceneManagement;
 // https://flowtree.tistory.com/46
 // https://wolstar.tistory.com/5
 
