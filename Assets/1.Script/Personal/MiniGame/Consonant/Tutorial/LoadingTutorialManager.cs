6cd28c27a716a5953ea44ce21c3fcb58be7d149b
HyeonJin67 <dudhkd0607@naver.com>
2022-11-23 17:52:02 +0900

    튜토리얼 스크립트에 주석달기

--- Assets/1.Script/Personal/MiniGame/Consonant/Tutorial/LoadingTutorialManager.cs
+++ Assets/1.Script/Personal/MiniGame/Consonant/Tutorial/LoadingTutorialManager.cs
@@ -10,29 +10,34 @@ using UnityEngine.SceneManagement;
 public class LoadingTutorialManager : MonoBehaviour
 {
     [SerializeField]
-    TutorialObjects tutorialObjects;
-    Vector3 endposition = new Vector3(5, -2, 0);
-    Vector3 startposition;
+    TutorialObjects tutorialObjects; //튜토리얼에 등장할 오브젝트들을 가지고 있는 Scriptable Object
+    Vector3 startposition; //Lerp에서 사용할 시작위치 고정용으로 첫 위치를 담아둘 변수
+    [SerializeField] 
+    GameObject[] objects; //없어질 예정
     [SerializeField]
-    GameObject[] objects;
-    [SerializeField]
-    AnimationCurve curve;
-    [SerializeField]
-    Transform pipe;
+    Transform pipe; //파이프 밑에 Lerp의 도착위치
    
-    public Transform arrive;
-    float lerpTime;
-    float currentTime;
-    float move;
+    public Transform arrive; //Lerp의 도착위치
+    float lerpTime; //Lerp의 도착때까지 총 소요시간
+    float currentTime; //등속운동을 위한 델타타임 저장용
 
     int i = 0;
-    float term = 0;
-    bool check = false;
-    float plusTime = 3;
-    int count = 0;
-    public static string nextScene;
+    float term = 0; //오브젝트별로 일렬로 세우기 위한 Lerp의 도착위치 간격 조절용
+    float plusTime = 3; //오브젝트별로 일렬로 세우기 위한 Lerp의 속도 조절용
+
+    public static string nextScene; //씬재생 지연함수가 끝나면(튜토리얼이 끝나면) 재생할 씬의 이름 저장용
     
+    //공부용
     //Animator anim;
+    /*    private void OnCollisionEnter(Collision collision)
+        {
+            print(collision.contacts[0].point); //충돌체 위치 포인트?
+        }
+    /*    private void sh()
+        {
+            anim.SetInteger(anim.GetComponent<Animator>().GetParameter(0).name,1); //애니메이터 파라미터 값 가져오기
+        }*/
+    //객체 생성없이 바로 갖다 쓸수 있는 정적(static) 함수
     public static void LoadScene(string sceneName)
     {
         print("씬이름 체크:" + sceneName);
@@ -59,7 +64,6 @@ public class LoadingTutorialManager : MonoBehaviour
     }
     private void Start()
     {
-        //for(i = 0 ; i < tutorialObjects.SaveObjects.Length; i++)
         print(tutorialObjects.GiyeokObjects[0].Object);
         print(tutorialObjects.GiyeokObjects[0].perAudio);
         print(tutorialObjects.GiyeokObjects.Length);
@@ -67,15 +71,15 @@ public class LoadingTutorialManager : MonoBehaviour
         //print(IntroductionObjects.);
 
         //StartCoroutine(Begin());
+        //동시에 Lerp로 움직이게 하기 위해 for문으로 빠르게 돌려서 코루틴 호출하기 위한 것
         for (int i = 0; i < objects.Length; i++)
         {
-            startposition = objects[i].transform.position;
+            startposition = objects[i].transform.position; //시작위치 고정용
             StartCoroutine(Move1(i, startposition, term, plusTime));
-            term += 3f;
-            plusTime += 5f;  
+            term += 3f; //일렬 간격 조정용
+            plusTime += 5f; //동시에 움직임으로 속도 조절로 일렬 유지
         }
     }
-
     //void Update()
     //{
     //    currentTime += Time.deltaTime;
@@ -83,15 +87,6 @@ public class LoadingTutorialManager : MonoBehaviour
     //    //transform.position = Vector3.Lerp(startposition, endposition, Mathf.SmoothStep(0,1,currentTime / lerpTime));
     //    //transform.position = Vector3.Lerp(startposition, endposition, curve.Evaluate(currentTime / lerpTime));
     //}
-
-    /*    private void OnCollisionEnter(Collision collision)
-        {
-            print(collision.contacts[0].point);
-        }
-    /*    private void sh()
-        {
-            anim.SetInteger(anim.GetComponent<Animator>().GetParameter(0).name,1);
-        }*/
 
     //IEnumerator Delay()
     //{
@@ -105,16 +100,16 @@ public class LoadingTutorialManager : MonoBehaviour
     //    }
     //}
 
-    //코루틴 호출을 지연
+    //코루틴 호출을 지연(for문이랑 다르게 3초 간격으로 코루틴을 천천히 호출하기 위한 지연 함수)
     IEnumerator Begin()
     {
-        while(i < objects.Length) //세번만
+        while(i < objects.Length) //오브젝트 갯수만큼만 반복
         {
             startposition = objects[i].transform.position;
             //StartCoroutine(Move1(i, startposition, term));
             i++;
             term += 3f;
-            yield return new WaitForSeconds(3);
+            yield return new WaitForSeconds(3); //3초 기다리기
         }
         //StartCoroutine(MoveOrdered());
     }
@@ -122,30 +117,26 @@ public class LoadingTutorialManager : MonoBehaviour
     {
         lerpTime = plusLerpTime;
         currentTime = 0;
-        while ((currentTime / lerpTime) <= 1)
+        while ((currentTime / lerpTime) <= 1) //https://www.youtube.com/watch?v=MyVY-y_jK1I&t=346s //Lerp의 등속운동
         {
             currentTime += Time.deltaTime;
             objects[index].transform.position = Vector3.Lerp(startPosi, new Vector3(arrive.position.x + term, arrive.position.y, arrive.position.z), currentTime / lerpTime);
             yield return null;
         }
-        yield return new WaitUntil(() => index == objects.Length - 1);
-        print("체크5");
+        yield return new WaitUntil(() => index == objects.Length - 1); //괄호안에 조건이 true가 될 때 밑에 줄 실행
         StartCoroutine(MoveOrdered());
     }
     //코루틴 호출을 지연
     IEnumerator MoveOrdered()
     {
         i = 0;
-        while (i < objects.Length) //세번만
+        while (i < objects.Length)
         {
             print(i + "번째 오브젝트 출발");
             startposition = objects[i].transform.position;
-            yield return StartCoroutine(Move3(i, startposition));
-            print("체크1");
+            yield return StartCoroutine(Move3(i, startposition)); //Move3코루틴이 끝나야지 밑에 줄 실행
             yield return new WaitForSeconds(6); //여기서 소개멘트 (플레이되는 시간만큼 지연)
-            print("체크2");
-            yield return StartCoroutine(Move4(i));
-            print("체크3");
+            yield return StartCoroutine(Move4(i)); //Move4코루틴이 끝나야지 밑에 줄 실행
             i++;
         }
     }
