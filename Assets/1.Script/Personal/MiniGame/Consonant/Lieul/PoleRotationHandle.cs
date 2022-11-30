6a0615321c89e27e816c1422b104b1e0dad4c631
HyeonJin67 <dudhkd0607@naver.com>
2022-11-30 15:22:49 +0900

    ㄹ씬의 버그 몇개 수정중
    잘못쓰던 lerp도 다시 수정

--- Assets/1.Script/Personal/MiniGame/Consonant/Lieul/PoleRotationHandle.cs
+++ Assets/1.Script/Personal/MiniGame/Consonant/Lieul/PoleRotationHandle.cs
@@ -8,9 +8,26 @@ public class PoleRotationHandle : MonoBehaviour
     //{
     //    transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * 500);
     //}
+    float lerpTime; //Lerp의 도착때까지 총 소요시간
+    float currentTime; //등속운동을 위한 델타타임 저장용
+    //Vector3 startposition; //Lerp에서 사용할 시작위치 고정용으로 첫 위치를 담아둘 변수
+    Quaternion startposition;
     private void Start()
     {
-        StartCoroutine(PoleRotate());
+        //StartCoroutine(PoleRotate());
+        StartCoroutine(Pole());
+    }
+    IEnumerator Pole()
+    {
+        lerpTime = 0.5f;
+        currentTime = 0;
+        startposition = transform.rotation;
+        while ((currentTime / lerpTime) < 1) //Lerp의 등속
+        {
+            currentTime += Time.deltaTime;
+            transform.rotation = Quaternion.Slerp(startposition, Quaternion.identity, currentTime / lerpTime);
+            yield return null;
+        }
     }
     IEnumerator PoleRotate()
     {
