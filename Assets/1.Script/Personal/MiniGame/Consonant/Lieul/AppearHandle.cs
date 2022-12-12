fa31001ccc1c78ba563e92f0a04a569a1d7e0f41
HyeonJin67 <dudhkd0607@naver.com>
2022-12-12 11:46:31 +0900

    Revert "OutLine 에셋 다운"
    This reverts commit 116d2cf387b93e2e8ead488f2671d5edd793bc0a.

--- Assets/1.Script/Personal/MiniGame/Consonant/Lieul/AppearHandle.cs
+++ Assets/1.Script/Personal/MiniGame/Consonant/Lieul/AppearHandle.cs
@@ -7,7 +7,6 @@ public class AppearHandle : MonoBehaviour
     float lerpTime = 2f; //등장시 소요시간
     float currentTime = 0;
     Vector3 start;
-    Vector3 startrotation;
     [SerializeField]
     Transform arrive; //도착지점
 
@@ -22,11 +21,6 @@ public class AppearHandle : MonoBehaviour
             animator.SetInteger("Lieul_Quiz", 1);
         }
     }
-    //void Update()
-    //{
-    //    currentTime += Time.deltaTime;
-    //    transform.rotation = Quaternion.Slerp(transform.rotation, transform.rotation, currentTime / lerpTime);
-    //}
     IEnumerator Appear()
     {
         while (currentTime / lerpTime < 1)
@@ -38,10 +32,7 @@ public class AppearHandle : MonoBehaviour
         if (GetComponent<Animator>())
         {
             animator = GetComponent<Animator>();
-            if (animator.parameterCount != 0)
-                animator.SetInteger("Lieul_Quiz", 2);
-            else
-                animator.enabled = false;
+            animator.SetInteger("Lieul_Quiz", 2);
         }
     }
 }
