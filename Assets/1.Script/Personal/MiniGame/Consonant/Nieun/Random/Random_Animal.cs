3009acdce3d1848a2f8f353c74c74f5ea4e5851b
Leesuhwa <dltnghks3256@gmail.com>
2022-12-27 09:41:07 +0900

    dd
    d

--- Assets/1.Script/Personal/MiniGame/Consonant/Nieun/Random/Random_Animal.cs
+++ Assets/1.Script/Personal/MiniGame/Consonant/Nieun/Random/Random_Animal.cs
@@ -6,7 +6,6 @@ using System.Linq;
 public class Random_Animal : MonoBehaviour
 {
     public int[] shuffledArray; //랜덤 배열
-    //기역 니은 오브젝트 자식의 자식 오브젝트 갯수 만큼 랜덤 배열 저장
     private void Awake()
     {
         var indexArray = Enumerable.Range(0, transform.GetChild(0).transform.GetChild(0).childCount).ToArray();
--- Assets/1.Script/Personal/MiniGame/Consonant/Nieun/Random/Random_AnimalChoice.cs
+++ Assets/1.Script/Personal/MiniGame/Consonant/Nieun/Random/Random_AnimalChoice.cs
@@ -4,12 +4,9 @@ using UnityEngine;
 
 public class Random_AnimalChoice : MonoBehaviour
 {
-    //Random_Animal 직렬화 
     [SerializeField]
     Random_Animal Animal;
-    //몇번째 동물인지 number숫자로 체크
     public int number;
-    //부모의 부모 Random_Animal 스크립트의 랜덤 배열 번호에 따라 자기 자신 밑의 오브젝트 활성화
     private void Start()
     {
         Animal = transform.parent.GetComponentInParent<Random_Animal>();
